e7017c4e44e7af546d20bd886d52f513b1a67de9
OleksandrHoloveshko <69433893+OleksandrHoloveshko@users.noreply.github.com>
2022-03-19 14:49:49 +0200

    Implemented Component Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import './App.css';
 
 const App = () => {
   return <>
     <BrowserRouter>
+      <MainNavigation/>
       <main>
         <Routes>
           <Route path="/" exact/>
