c9c229175061b056e2212432925d3f2567d1dbed
OleksandrHoloveshko <69433893+OleksandrHoloveshko@users.noreply.github.com>
2022-03-20 11:13:41 +0200

    Add Component Places

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
+import UserPlaces from "./places/pages/UserPlaces";
 import './App.css';
+
 
 const App = () => {
   return <>
@@ -10,6 +12,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" exact element={<Users/>}/>
+          <Route path="/:userId/places" exact element={<UserPlaces/>}/>
           <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </main>
