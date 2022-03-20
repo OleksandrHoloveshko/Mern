b65c2fd3695934d7ce5315e15a13a97aa5796a9a
OleksandrHoloveshko <69433893+OleksandrHoloveshko@users.noreply.github.com>
2022-03-20 12:49:56 +0200

    Create Component Button

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Card from '../../shared/components/UIElements/Card';
+import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
 import './PlaceItem.css';
 
@@ -9,6 +10,7 @@ const PlaceItem = props => {
     const openMapHandler = () => setShowMap(true);
 
     const closeMapHandler = () => setShowMap(false);
+
     return (
         <>
             <Modal
@@ -17,7 +19,7 @@ const PlaceItem = props => {
                 header={props.address}
                 contentClass="place-item__modal-content"
                 footerClass="place-item__modal-actions"
-                footer={<button onClick={closeMapHandler}>CLOSE</button>}
+                footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
             >
                 <div className="map-container">
                     <h2>THE MAP!</h2>
@@ -34,9 +36,9 @@ const PlaceItem = props => {
                         <p>{props.description}</p>
                     </div>
                     <div className="place-item__actions">
-                        <button onClick={openMapHandler}>VIEW ON MAP</button>
-                        <button>EDIT</button>
-                        <button>DELETE</button>
+                        <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+                        <Button to={`/places/${props.id}`}>EDIT</Button>
+                        <Button danger>DELETE</Button>
                     </div>
                 </Card>
             </li>
